7f3f601e7cd3300bd155f54a2c061ab2d396428a
riya-analytics <r11076675@gmail.com>
2026-03-12 06:37:28 +0000

    First flask project saved!

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,11 @@
-def sum(a, b):
-    return a+b
+from flask import Flask, render_template
 
-print(sum(5,6))
-print("Hey Riya your code has executed!!!")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
